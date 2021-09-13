f5b9fc28cec9f1d7501d4010d8115af4836e4e83
StefanNijssen <58977715+StefanNijssen@users.noreply.github.com>
2021-09-13 10:25:06 +0200

    Dagen

---
+++ duizelig/dagen.py
@@ -0,0 +1,37 @@
+Ma = "Ma"
+Di = "Di"
+Wo = "Wo"
+Do = "Do"
+Vr = "Vr"
+Za = "Za"
+Zo = "Zo"
+Dag = input("welke dag wilt u weten?")
+while True:
+    if Ma == Dag:
+        break
+    else:
+        print("Maandag")
+    if Di == Dag:
+        break
+    else:
+        print("Dinsdag")
+    if Wo == Dag:
+        break
+    else:
+        print("Woensdag")
+    if Do == Dag:
+        break
+    else:
+        print("Donderdag")
+    if Vr == Dag:
+        break
+    else:
+        print("Vrijdag")
+    if Za == Dag:
+        break
+    else:
+        print("Zaterdag")
+    if Zo == Dag:
+        break
+    else:
+        print("Zondag")
