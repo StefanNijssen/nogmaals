ce1afeae8a8408f7565678a88658a10bb4d5ef3d
StefanNijssen <58977715+StefanNijssen@users.noreply.github.com>
2021-09-20 13:40:10 +0200

    new

--- Robot/example13.py
+++ Robot/example13.py
@@ -4,28 +4,19 @@ robotArm.randomLevel(1,7)
 
 i = 0
 x = 1
-robotArm.grab()
-color = robotArm.scan()
-color
-for i in range(1,10):
-    if color == "red" or "white" or "green" or "blue" or "yellow":
-        robotArm.grab()
+
+while True:
+    robotArm.grab()
+    color = robotArm.scan()
+    if color != "":
         x += 1
         for i in range(1,x):
             robotArm.moveRight()
         robotArm.drop()
     else:
-        robotArm.wait()
+        break       
     for i in range(1,x):
         robotArm.moveLeft()
 
+robotArm.wait()
 
-
-
-
-robotArm.operate()
-robotArm.grab()
-robotArm.moveRight()
-robotArm.moveLeft()
-robotArm.scan()
-robotArm.drop()
